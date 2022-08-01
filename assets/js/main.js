8815adb986932b63f31fcee3886ac7a4520152cd
Jordane <jordan2offshel@hotmaile.com>
2022-08-01 17:54:24 -0400

    Developed by Zertox-dev

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 
 
+// Event click
 const toggle = () => {
     var menu = document.querySelector(".nav__list");
     var menuBtn = document.querySelector(".nav__humbger");
 
-    menuBtn.addEventListener("click", function() {
+    menuBtn.addEventListener("click", function () {
         menuBtn.classList.toggle("active");
         menu.classList.toggle("active");
     })
